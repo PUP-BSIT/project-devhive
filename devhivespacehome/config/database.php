bf6639539f680f3adf607b416ef144b7f9ee7dbe
Hannah Lorainne Genandoy <lorainneh540@gmail.com>
2025-07-08 15:32:59 +0800

    [modify] fix for get recent post in dashboard (#159)

--- devhivespacehome/config/database.php
+++ devhivespacehome/config/database.php
@@ -4,11 +4,21 @@ $db_name = "u798703225_devhivespace";
 $username = "u798703225_devhivespace";
 $password = "Pupt2026";
 
-// Create connection
+// For PDO
+$dsn = "mysql:host=$host;dbname=$db_name;charset=utf8mb4";
+$db_user = $username;
+$db_pass = $password;
+
 $conn = new mysqli($host, $username, $password, $db_name);
 
-// Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    error_log("Database Connection Error: " . $conn->connect_error);
+
+    session_start();
+    session_unset();
+    session_destroy();
+    
+    header("Location: /login/index.html");
+    exit;
 }
 ?>
